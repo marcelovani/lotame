a1879d681856761281692e95f82118b7d7dc750e
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-08-16 15:07:19 +0100

    Removed custom tokens and simplyfied the code.

--- modules/lotame_adv_tags/lotame_adv_tags.api.php
+++ modules/lotame_adv_tags/lotame_adv_tags.api.php
@@ -13,8 +13,6 @@ function hook_lotame_adv_tags_default_patterns() {
   $patterns = array(
     // You can use text and drupal tokens.
     'global' => 'Custom^Technology^site^^[site:name]',
-    // If you have dfp enabled, you can add Global targeting keys enclosed by {}.
-    'articles' => 'Custom^Technology^DFP^^{key}',
   );
 
   return $patterns;
